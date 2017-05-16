579873044715040d51b37ca5957cab87cf4e92d3
whitzhu <whitzhu@gmail.com>
2017-05-16 14:43:09 -0700

    longestSubstring optimize solution

--- longestSubstring/longestSubstring.js
+++ longestSubstring/longestSubstring.js
@@ -11,26 +11,42 @@ Given "bbbbb", the answer is "b", with the length of 1.
 Given "pwwkew", the answer is "wke", with the length of 3. 
 Note that the answer must be a substring, "pwke" is a 
 subsequence and not a substring.
-
 */
 
-const lengthOfLongestSubstring = function(string) {
-  string = string.split('');
-  let max_length = 0; 
-  let visited = [];
 
-  string.forEach( (char, index) => {
-    if (visited.indexOf(char) === -1) {
-      visited.push(char);
-      visited.length > max_length ? max_length = visited.length : null;
+/*Brute Force Solution*/
+// const lengthOfLongestSubstring = function(string) {
+//   string = string.split('');
+//   let max_length = 0; 
+//   let visited = [];
+
+//   string.forEach( (char, index) => {
+//     if (visited.indexOf(char) === -1) {
+//       visited.push(char);
+//       visited.length > max_length ? max_length = visited.length : null;
+//     } else {
+//       let indexOfNextChar = visited.lastIndexOf(char)+1;
+//       visited.length > max_length ? max_length = visited.length : null;       
+//       visited = visited.slice(indexOfNextChar);
+//       visited.push(char);
+//     }
+//   })
+//   return max_length;
+// };
+
+
+const lengthOfLongestSubstring = function(s) {
+  let start = 0;
+  let storage = {};
+  let maxLength = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] in storage && start <= storage[s[i]]) { 
+      start = storage[s[i]] + 1;
     } else {
-      let indexOfNextChar = visited.lastIndexOf(char)+1;
-      visited.length > max_length ? max_length = visited.length : null;       
-      visited = visited.slice(indexOfNextChar);
-      visited.push(char);
+      maxLength = Math.max(maxLength, i - start + 1);
     }
-  })
-  return max_length;
-};
-
-
+    storage[s[i]] = i;
+  }
+  return maxLength;
+}
--- longestSubstring/longestSubstringTest.js
+++ longestSubstring/longestSubstringTest.js
@@ -7,33 +7,45 @@ describe('Longest Substring', function() {
     assert.equal(longestSubstring, 3);
   })
 
-  it('should return length of longest substring', function() {
+  it('should return length of longest substring for "aab"', function() {
     const string = "aab";
     let longestSubstring = lengthOfLongestSubstring(string);
     assert.equal(longestSubstring, 2);
   })
 
-  it('should return length of longest substring', function() {
+  it('should return length of longest substring for "dvdf"', function() {
     const string = "dvdf";
     let longestSubstring = lengthOfLongestSubstring(string);
     assert.equal(longestSubstring, 3);
   })  
 
-  it('should return length of longest substring', function() {
+  it('should return length of longest substring "pwwkew"', function() {
     const string = "pwwkew";
     let longestSubstring = lengthOfLongestSubstring(string);
     assert.equal(longestSubstring, 3);
   })  
 
-  it('should return length of longest substring', function() {
+  it('should return length of longest substring for "asjrgapa"', function() {
     const string = "asjrgapa";
     let longestSubstring = lengthOfLongestSubstring(string);
     assert.equal(longestSubstring, 6);
   })  
 
-  it('should return length of longest substring', function() {
+  it('should return length of longest substring "c"', function() {
     const string = "c";
     let longestSubstring = lengthOfLongestSubstring(string);
     assert.equal(longestSubstring, 1);
+  })  
+
+  it('should return length of longest substring "abba"', function() {
+    const string = "abba";
+    let longestSubstring = lengthOfLongestSubstring(string);
+    assert.equal(longestSubstring, 2);
+  })  
+
+  it('should return length of longest substring "tmmzuxt"', function() {
+    const string = "tmmzuxt";
+    let longestSubstring = lengthOfLongestSubstring(string);
+    assert.equal(longestSubstring, 5);
   })
 })
