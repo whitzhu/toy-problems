ac464894f261f83abfaf7fa28aef2a3828aff1dd
whitzhu <whitzhu@gmail.com>
2017-05-25 13:03:56 -0700

    [sorting] merge sort

---
+++ sorting/mergeSort.js
@@ -0,0 +1,25 @@
+const mergeSort = function(list) {  
+  if (list.length < 2) return list;
+  
+  let left = list.slice(0, list.length / 2); 
+  let right = list.slice(list.length / 2);   
+
+  left = mergeSort(left);
+  right = mergeSort(right);
+
+  return merge(left, right);
+}
+
+const merge = function(left, right) {
+  let lpointer = 0, rpointer = 0, result = [];
+
+  while (result.length < (left.length + right.length)) {
+    if (lpointer === left.length) result = result.concat(right.slice(rpointer));
+    else if ( rpointer === right.length) result = result.concat(left.slice(lpointer));
+    else if ( left[lpointer] < right[rpointer]) result.push(left[lpointer++]);
+    else result.push(right[rpointer++]);
+  }
+  return result;
+}
+
+//Time Complexity O(n logn)
