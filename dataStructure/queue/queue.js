c22252331d19447e81c5e47cbda38946c315c392
whitzhu <whitzhu@gmail.com>
2017-06-05 20:25:27 -0700

    queue.js - remove console logs

--- dataStructure/queue/queue.js
+++ dataStructure/queue/queue.js
@@ -8,18 +8,13 @@ const Queue = function() {
 Queue.prototype.enqueue = function(val) {
   this._storage[this._end] = val;
   this._end++;
-  console.log('end is ', this._end);
 }
 
 Queue.prototype.dequeue = function(val) {
   if ( this._end - this._start > 0) {
     let remove = this._storage[this._start];
     delete this._storage[this._start];
-    console.log('start is before adding', this._start);
     this._start++;
-    console.log('stoage is ', this._storage);
-    console.log('start is ', this._start);
-    console.log('remove is', remove);
     return remove;
   } else {
     return null;
