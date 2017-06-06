42658b3348786c59095bf6aab94c9837c5530b6d
whitzhu <whitzhu@gmail.com>
2017-06-05 20:25:58 -0700

    dataStructureSpecs - update to use ES6

--- dataStructure/specs/dataStructureSpecs.js
+++ dataStructure/specs/dataStructureSpecs.js
@@ -1,46 +1,46 @@
 const assert = chai.assert;
 const expect = chai.expect;
 
-describe('stack', function() {
+describe('stack', () => {
   let stack = new Stack();
 
-  it('should report a size of zero for a new stack', function() {
+  it('reports a size of zero for a new stack', () => {
     expect(stack.size()).to.equal(0);
   })
 
-  it('should report a size of 2 after adding two items', function() {
+  it('reports a size of 2 after adding two items', () => {
     stack.push('a');
     stack.push('b');
     expect(stack.size()).to.equal(2);
   })
 
-  it('should not error when removing from an empty stack', function() {
+  it('does not error when removing from an empty stack', () => {
     stack.pop();
-    expect(function() { stack.pop();}).not.throws();
+    expect(() => { stack.pop();}).not.throws();
   })
 
-  it('should report a size of 1 after adding two items and removing one', function() {
+  it('reports a size of 1 after adding two items and removing one', () => {
     stack.push('a');
     stack.push('b');
     stack.pop();
     expect(stack.size()).to.equal(1);
   })
 
-  it('should report a size of 0 after adding removing more items than were added', function() {
+  it('reports a size of 0 after adding removing more items than were added', () => {
     stack.push('a');
     stack.pop();
     stack.pop();
     expect(stack.size()).to.equal(0);
   })
 
-  it('should sequentially add and remove items', function() {
+  it('sequentially add and remove items', () => {
     stack.push('a');
     expect(stack.pop()).to.equal('a');    
     stack.push('b');
     expect(stack.pop()).to.equal('b');
   })  
 
-  it('should remove the newest item, after newer items have already been added and removed ', function() {
+  it('removes the newest item, after newer items have already been added and removed ', () => {
     stack.push('a');
     stack.push('b');
     stack.push('c');
@@ -49,15 +49,15 @@ describe('stack', function() {
   })
 })
 
-describe('queue', function() {
+describe('queue', () => {
   let queue = new Queue();
 
-  it('should report a size of zero for a new queue', function() {
+  it('reports a size of zero for a new queue', () => {
     expect(queue.size()).to.equal(0);
   })  
 
 
-  it('should report a size of 1 after adding two items and removing one', function() {
+  it('reports a size of 1 after adding two items and removing one', () => {
     queue.enqueue('hello');
     queue.enqueue('world');
     queue.dequeue();
@@ -65,32 +65,32 @@ describe('queue', function() {
     queue.dequeue(); //empty queue
   })
 
-  it('does not error when removing from an empty queue', function() {
-    expect(function() { queue.dequeue();}).not.throws();
+  it('does not error when removing from an empty queue', () => {
+    expect(() => { queue.dequeue();}).not.throws();
   })  
 
-  it('should report a size of 0 after removing more items than were added', function() {
+  it('report a size of 0 after removing more items than were added', () => {
     queue.enqueue('a');
     queue.dequeue();
     queue.dequeue();
     expect(queue.size()).to.equal(0);
   })
 
-  it('should allow sequentially adding and removing items', function() {
+  it('allow sequentially adding and removing items', () => {
     queue.enqueue('a');
     expect(queue.dequeue()).to.equal('a');
     queue.enqueue('b');
     expect(queue.dequeue()).to.equal('b');
   })
 
-  it('should remove the least recently added of two items', function() {
+  it('removes the least recently added of two items', () => {
     queue.enqueue('a');
     queue.enqueue('b');
     expect(queue.dequeue()).to.equal('a');
     queue.dequeue(); //empty queue
   })
 
-  it('should remove the oldest item, after newer items have already been added and removed', function() {
+  it('removes the oldest item, after newer items have already been added and removed', () => {
     queue.enqueue('a');
     queue.enqueue('b');
     queue.dequeue();
